240b88268092f1915263a17cf643daf683b4e930
Qiqi Jin <ginqi7@gmail.com>
2025-12-18 19:19:17 +0800

    中文博客

---
+++ _data/project.js
@@ -0,0 +1,11 @@
+export default function () {
+  return {
+    currentYear: new Date().getFullYear(),
+    title: "Hello World",
+    author: "Qiqi Jin",
+    mail: "ginqi7@gmail.com",
+    github: "https://github.com/ginqi7",
+    indexPostSize: 10,
+    baseURL: "/zh-blog/",
+  };
+}
